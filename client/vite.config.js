4972e21400e001635f71414da61045cc1ee3c2ba
Selvakumar Balraj <selvakumar.b@keenstack.com>
2025-08-06 12:58:32 +0530

    port issues

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0',       // Allow external connections
-    port: process.env.PORT || 5173,  // Use PORT from Render
+    host: '0.0.0.0',                       // <-- required
+    port: parseInt(process.env.PORT) || 5173,  // <-- required for Render
+  },
+  build: {
+    outDir: 'dist',
   },
 })
+
 
