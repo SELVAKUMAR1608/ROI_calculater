56ffb6979092022fa1a33eadcd33820587647ad7
Selvakumar Balraj <selvakumar.b@keenstack.com>
2025-08-06 14:21:47 +0530

    789

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,13 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: '0.0.0.0',                       // <-- required
-    port: parseInt(process.env.PORT) || 5173,  // <-- required for Render
-  },
   build: {
-    outDir: 'dist',
+    outDir: 'dist', // important: this is what Vercel will deploy
   },
 })
+
 
 
